b2567ab08e3a664b9ce3f80b365eb92ad887f1d7
gauravgaikwad242 <gaikwadg576@gmail.com>
2022-06-04 12:43:38 +0530

    added list to the tester

--- src/com/infy/Tester.java
+++ src/com/infy/Tester.java
@@ -1,9 +1,13 @@
 package com.infy;
 
+import java.util.List;
+
 public class Tester {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		List<String> list = List.of("gaurav", "gaikwad");
+		
 	}
 	
 }
